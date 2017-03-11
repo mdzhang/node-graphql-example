92c12e1d2b9840c97b13d79cc2988a8380b64301
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-03-11 18:29:49 -0500

    Add pm2
    Only really tested start + live reload in development mode

---
+++ pm2.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const _ = require('lodash');
+
+const defaultConfig = {
+  apps: {
+    api: {
+      name: 'api',
+      env: {
+        NODE_ENV: process.env.ENV,
+        PORT: 8080,
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      script: './src/index.js',
+    },
+  },
+};
+
+const configByEnv = {
+  development: {
+    apps: {
+      api: {
+        exec_interpreter: 'babel-node',
+        max_restarts: 2,
+        watch: [
+          'node_modules',
+          'src',
+        ],
+      },
+    },
+  },
+  production: {
+    apps: {
+      api: {
+        exec_mode: 'cluster',
+        instances: 0,
+        script: './dist/index.js',
+      },
+    },
+  },
+};
+
+const config = _.defaultsDeep(configByEnv[process.env.ENV], defaultConfig);
+
+module.exports = {
+  apps: [
+    config.apps.api,
+  ],
+};
